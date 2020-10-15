2a25ccd4fc7d8a6969665781652cdc5db37ad3e5
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-15 05:33:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each do
+  array.each_with_index do
     |element|
-    element.each_with_index
   end
   # Use `each_with_index` to enumerate over the provided array
   #
