0fe1251899331fe862c51d3dc97905c39027aa64
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-15 05:41:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do
-    |element,index|
+    |element,index=1|
     puts "#{index}. #{element}"
   end
   # Use `each_with_index` to enumerate over the provided array
